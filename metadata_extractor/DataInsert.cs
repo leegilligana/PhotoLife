b3646741c1f2c7c10205cb0a76c92a25c9f09819
AlejandroG31 <alejo02385@gmail.com>
2024-02-07 19:29:12 -0600

    Added user paramter

--- metadata_extractor/DataInsert.cs
+++ metadata_extractor/DataInsert.cs
@@ -57,16 +57,17 @@ namespace metadata_extractor
 			return new string[] { insertString, valuesString };
 		}	
 
-		public void insertData(Dictionary<string,string> data, string[] quoteList, string[] noQuoteList)
+		public void insertData(Dictionary<string,string> data, string[] quoteList, string[] noQuoteList, string owner)
 		{
 			var arr1 = noQuote(data, noQuoteList);
 			var arr2 = withQuote(data, quoteList);
-			var command = $"INSERT INTO test3 ({arr1[0] + arr2[0]}) VALUES " +
-						$"({arr1[1] + arr2[1]});";
+			var command = $"INSERT INTO test3 (Owner, {arr1[0] + arr2[0]}) VALUES " +
+						$"('@owner', {arr1[1] + arr2[1]});";
 			Console.WriteLine(command);
 			using (var cmd = new NpgsqlCommand(
 						command, conn))
 			{
+				cmd.Parameters.AddWithValue("Owner", owner);
 				cmd.ExecuteNonQuery();
 			};
 			conn.Close();
--- metadata_extractor/Queries.cs
+++ metadata_extractor/Queries.cs
@@ -13,33 +13,38 @@ namespace DB_Queries
 	internal class Queries
 	{
 		private NpgsqlConnection conn;
-		public Queries(string connString)
+		private string user;
+		public Queries(string connString, string owner)
 		{
 			var dataSourceBuilder = new NpgsqlDataSourceBuilder(connString);
 			var dataSource = dataSourceBuilder.Build();
 			conn = dataSource.OpenConnection();
+			user = owner;
 
 		}
 
 		public string[] Flash(bool isFlash)
 		{
 
-			var cmd = new NpgsqlCommand("SELECT file_name FROM test3 WHERE Flash = @isFlash;", conn);
+			var cmd = new NpgsqlCommand("SELECT file_name FROM test3 WHERE User = @user AND Flash = @isFlash;", conn);
 			cmd.Parameters.AddWithValue("isFlash", isFlash);
+			cmd.Parameters.AddWithValue("user", user);
 			return DataReader(cmd.ExecuteReader());
 		}
 
-		public string[] Model(string model)
+		public string[] Model(string model, string user)
 		{
-			var cmd = new NpgsqlCommand($"SELECT file_name FROM test3 WHERE model LIKE '%{model}%';", conn);
+			var cmd = new NpgsqlCommand($"SELECT file_name FROM test3 WHERE User = @user AND model LIKE '%{model}%';", conn);
+			cmd.Parameters.AddWithValue("user", user);
 			return DataReader(cmd.ExecuteReader());
 		}
 
 		public string[] DateTimeRange(DateTime start, DateTime end)
 		{
-			var cmd = new NpgsqlCommand("SELECT file_name FROM test3 WHERE date_time >= @start AND date_time <= @end;", conn);
+			var cmd = new NpgsqlCommand("SELECT file_name FROM test3 WHERE User = @user AND date_time >= @start AND date_time <= @end;", conn);
 			cmd.Parameters.AddWithValue("start", start);
 			cmd.Parameters.AddWithValue("end", end);
+			cmd.Parameters.AddWithValue("user", user);
 			return DataReader(cmd.ExecuteReader());
 		}
 
@@ -47,8 +52,8 @@ namespace DB_Queries
 		{
 			var locList = new List<string>();
 			GeoCoordinate inputCoordinate = new GeoCoordinate(inputLat, inputLong);
-			var cmd = new NpgsqlCommand("SELECT file_name, gps_coordinates FROM test3;", conn);
-
+			var cmd = new NpgsqlCommand("SELECT file_name, gps_coordinates FROM test3 WHERE User = @user;", conn);
+			cmd.Parameters.AddWithValue("user", user);
 			using (NpgsqlDataReader reader = cmd.ExecuteReader())
 			{
 				// Iterate over the rows
@@ -87,124 +92,141 @@ namespace DB_Queries
 
 		public string[] Width(int width)
 		{
-			var cmd = new NpgsqlCommand("SELECT file_name FROM test3 WHERE image_width = @width;", conn);
+			var cmd = new NpgsqlCommand("SELECT file_name FROM test3 WHERE User = @user AND image_width = @width;", conn);
 			cmd.Parameters.AddWithValue("width", width);
+			cmd.Parameters.AddWithValue("user", user);
 			return DataReader(cmd.ExecuteReader());
 		}
 
 		public string[] Height(int height)
 		{
-			var cmd = new NpgsqlCommand("SELECT file_name FROM test3 WHERE image_height = @height;", conn);
+			var cmd = new NpgsqlCommand("SELECT file_name FROM test3 WHERE User = @user AND image_height = @height;", conn);
 			cmd.Parameters.AddWithValue("height", height);
+			cmd.Parameters.AddWithValue("user", user);
 			return DataReader(cmd.ExecuteReader());
 		}
 
 		public string[] Orientation(string orientation)
 		{
-			var cmd = new NpgsqlCommand($"SELECT file_name FROM test3 WHERE orientation LIKE '%{orientation}%';", conn);
+			var cmd = new NpgsqlCommand($"SELECT file_name FROM test3 WHERE User = @user AND orientation LIKE '%{orientation}%';", conn);
+			cmd.Parameters.AddWithValue("user", user);
 			return DataReader(cmd.ExecuteReader());
 		}
 
 		public string[] XResolution(int xRes)
 		{
-			var cmd = new NpgsqlCommand("SELECT file_name FROM test3 WHERE x_resolution = @xRes;", conn);
+			var cmd = new NpgsqlCommand("SELECT file_name FROM test3 WHERE User = @user AND x_resolution = @xRes;", conn);
 			cmd.Parameters.AddWithValue("xRes", xRes);
+			cmd.Parameters.AddWithValue("user", user);
 			return DataReader(cmd.ExecuteReader());
 		}
 
 		public string[] YResolution(int yRes)
 		{
-			var cmd = new NpgsqlCommand("SELECT file_name FROM test3 WHERE y_resolution = @yRes;", conn);
+			var cmd = new NpgsqlCommand("SELECT file_name FROM test3 WHERE User = @user AND y_resolution = @yRes;", conn);
 			cmd.Parameters.AddWithValue("yRes", yRes);
+			cmd.Parameters.AddWithValue("user", user);
 			return DataReader(cmd.ExecuteReader());
 		}
 
 		public string[] Software(string software)
 		{
-			var cmd = new NpgsqlCommand($"SELECT file_name FROM test3 WHERE software LIKE '%{software}%';", conn);
+			var cmd = new NpgsqlCommand($"SELECT file_name FROM test3 WHERE User = @user AND software LIKE '%{software}%';", conn);
+			cmd.Parameters.AddWithValue("user", user);
 			return DataReader(cmd.ExecuteReader());
 		}
 
 		public string[] ExposureTime(double exposureTime)
 		{
-			var cmd = new NpgsqlCommand("SELECT file_name FROM test3 WHERE exposure_time = @exposureTime;", conn);
+			var cmd = new NpgsqlCommand("SELECT file_name FROM test3 WHERE User = @user AND exposure_time = @exposureTime;", conn);
 			cmd.Parameters.AddWithValue("exposureTime", exposureTime);
+			cmd.Parameters.AddWithValue("user", user);
 			return DataReader(cmd.ExecuteReader());
 		}
 
 		public string[] ShutterSpeedValue(double shutterSpeedValue)
 		{
-			var cmd = new NpgsqlCommand("SELECT file_name FROM test3 WHERE shutter_speed_value = @shutterSpeedValue;", conn);
+			var cmd = new NpgsqlCommand("SELECT file_name FROM test3 WHERE User = @user AND shutter_speed_value = @shutterSpeedValue;", conn);
 			cmd.Parameters.AddWithValue("shutterSpeedValue", shutterSpeedValue);
+			cmd.Parameters.AddWithValue("user", user);
 			return DataReader(cmd.ExecuteReader());
 		}
 
 		public string[] BrightnessValue(double brightnessValue)
 		{
-			var cmd = new NpgsqlCommand("SELECT file_name FROM test3 WHERE brightness_value = @brightnessValue;", conn);
+			var cmd = new NpgsqlCommand("SELECT file_name FROM test3 WHERE User = @user AND brightness_value = @brightnessValue;", conn);
 			cmd.Parameters.AddWithValue("brightnessValue", brightnessValue);
+			cmd.Parameters.AddWithValue("user", user);
 			return DataReader(cmd.ExecuteReader());
 		}
 
 		public string[] SceneType(string sceneType)
 		{
-			var cmd = new NpgsqlCommand($"SELECT file_name FROM test3 WHERE scene_type LIKE '%{sceneType}%';", conn);
+			var cmd = new NpgsqlCommand($"SELECT file_name FROM test3 WHERE User = @user AND scene_type LIKE '%{sceneType}%';", conn);
+			cmd.Parameters.AddWithValue("user", user);
 			return DataReader(cmd.ExecuteReader());
 		}
 
 		public string[] ExposureMode(string exposureMode)
 		{
-			var cmd = new NpgsqlCommand($"SELECT file_name FROM test3 WHERE exposure_mode LIKE '%{exposureMode}%';", conn);
+			var cmd = new NpgsqlCommand($"SELECT file_name FROM test3 WHERE User = @user AND exposure_mode LIKE '%{exposureMode}%';", conn);
+			cmd.Parameters.AddWithValue("user", user);
 			return DataReader(cmd.ExecuteReader());
 		}
 
 		public string[] LensModel(string lensModel)
 		{
-			var cmd = new NpgsqlCommand($"SELECT file_name FROM test3 WHERE lens_model LIKE '%{lensModel}%';", conn);
+			var cmd = new NpgsqlCommand($"SELECT file_name FROM test3 WHERE User = @user AND lens_model LIKE '%{lensModel}%';", conn);
 			return DataReader(cmd.ExecuteReader());
 		}
 
 		public string[] FileType(string fileType)
 		{
-			var cmd = new NpgsqlCommand($"SELECT file_name FROM test3 WHERE detected_file_type_name LIKE '%{fileType}%';", conn);
+			var cmd = new NpgsqlCommand($"SELECT file_name FROM test3 WHERE User = @user AND detected_file_type_name LIKE '%{fileType}%';", conn);
+			cmd.Parameters.AddWithValue("user", user);
 			return DataReader(cmd.ExecuteReader());
 		}
 
 		public string[] FileName(string fileName)
 		{
-			var cmd = new NpgsqlCommand($"SELECT file_name FROM test3 WHERE file_name LIKE '%{fileName}%';", conn);
+			var cmd = new NpgsqlCommand($"SELECT file_name FROM test3 WHERE User = @user AND file_name LIKE '%{fileName}%';", conn);
+			cmd.Parameters.AddWithValue("user", user);
 			return DataReader(cmd.ExecuteReader());
 		}
 
 		public string[] FileSize(float fileSize)
 		{
-			var cmd = new NpgsqlCommand("SELECT file_name FROM test3 WHERE file_size = @fileSize;", conn);
+			var cmd = new NpgsqlCommand("SELECT file_name FROM test3 WHERE User = @user AND file_size = @fileSize;", conn);
 			cmd.Parameters.AddWithValue("fileSize", fileSize);
+			cmd.Parameters.AddWithValue("user", user);
 			return DataReader(cmd.ExecuteReader());
 		}
 
 		public string[] GPSAltitude(double gpsAltitude)
 		{
-			var cmd = new NpgsqlCommand("SELECT file_name FROM test3 WHERE gps_altitude = @gpsAltitude;", conn);
+			var cmd = new NpgsqlCommand("SELECT file_name FROM test3 WHERE User = @user AND gps_altitude = @gpsAltitude;", conn);
 			cmd.Parameters.AddWithValue("gpsAltitude", gpsAltitude);
+			cmd.Parameters.AddWithValue("user", user);
 			return DataReader(cmd.ExecuteReader());
 		}
 
 		public string[] GPSImgDirection(double gpsImgDirection)
 		{
-			var cmd = new NpgsqlCommand("SELECT file_name FROM test3 WHERE gps_img_direction = @gpsImgDirection;", conn);
+			var cmd = new NpgsqlCommand("SELECT file_name FROM test3 WHERE User = @user AND gps_img_direction = @gpsImgDirection;", conn);
 			cmd.Parameters.AddWithValue("gpsImgDirection", gpsImgDirection);
+			cmd.Parameters.AddWithValue("user", user);
 			return DataReader(cmd.ExecuteReader());
 		}
 
 		public string[] GPSHorizontalPositioningError(double gpsHorizontalPositioningError)
 		{
-			var cmd = new NpgsqlCommand("SELECT file_name FROM test3 WHERE gps_horizontal_positioning_error = @gpsHorizontalPositioningError;", conn);
+			var cmd = new NpgsqlCommand("SELECT file_name FROM test3 WHERE User = @user AND gps_horizontal_positioning_error = @gpsHorizontalPositioningError;", conn);
 			cmd.Parameters.AddWithValue("gpsHorizontalPositioningError", gpsHorizontalPositioningError);
+			cmd.Parameters.AddWithValue("user", user);
 			return DataReader(cmd.ExecuteReader());
 		}
 
-		public void deleteAll(string user)
+		public void deleteAll()
 		{
 			var cmd = new NpgsqlCommand("DELETE FROM test3 where Owner = @user;", conn);
 			cmd.Parameters.AddWithValue("user", user);
